87f4621f8e4b1adee2a2add7f12647be9800487e
Diggo <rdiggosilva@gmail.com>
2024-10-15 12:16:17 -0300

    Created FeedViewFile and implemented tableView

--- Animals/App/Scenes/Feed/FeedViewController.swift
+++ Animals/App/Scenes/Feed/FeedViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class FeedViewController: UIViewController {
+    
+    let feedView = FeedView()
+    
+    override func loadView() {
+        super.loadView()
+        view = feedView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavBar()
         setDelegatesAndDataSources()
-        view.backgroundColor = .white
     }
     
     private func setNavBar() {
@@ -22,6 +28,56 @@ class FeedViewController: UIViewController {
     }
     
     private func setDelegatesAndDataSources() {
-        
+        feedView.tableView.delegate = self
+        feedView.tableView.dataSource = self
+    }
+}
+
+extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello World: \(indexPath.row)"
+        return cell
+    }
+}
+
+class FeedView: UIView {
+    lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy () {
+        backgroundColor = .systemBackground
+        addSubview(tableView)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
     }
 }
